d49df8c633e63f7db9beae60c64e610bc552e86f
xlemx <magdalena.lem011@o2.pl>
2022-04-11 12:48:14 +0100

    Ad.4.

--- src/main/java/com/example/lab2javafx/HelloController.java
+++ src/main/java/com/example/lab2javafx/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.lab2javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    public Button btnMain;
+    int clicCounter=0;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Kliknięcie nr "+ ++clicCounter);
+        if (clicCounter==1)
+            btnMain.setText("I jeszcze raz");
     }
 }
