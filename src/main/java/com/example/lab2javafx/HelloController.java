6081cdb50659929d78f2672834f383803503c0ca
xlemx <magdalena.lem011@o2.pl>
2022-04-11 13:39:19 +0100

    Ad.5

--- src/main/java/com/example/lab2javafx/HelloController.java
+++ src/main/java/com/example/lab2javafx/HelloController.java
@@ -4,10 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 
 public class HelloController {
     public Button btnMain;
+    public TextField txtLicznik;
     int clicCounter=0;
+    int textClicCounter=0;
+
     @FXML
     private Label welcomeText;
 
@@ -20,5 +25,11 @@ public class HelloController {
         System.out.println("Kliknięcie nr "+ ++clicCounter);
         if (clicCounter==1)
             btnMain.setText("I jeszcze raz");
+    }
+    public void txtLicznik(ActionEvent actionEvent){
+        System.out.println("Kliknięcie nr "+ ++textClicCounter);
+        if (textClicCounter==1)
+            txtLicznik.setText("");
+
     }
 }
