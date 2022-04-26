8dca9e2a6e0bc50daafcfbbe89a1a678287e35b0
xlemx <magdalena.lem011@o2.pl>
2022-04-26 19:19:30 +0100

    Ad.6

--- src/main/java/com/example/lab2javafx/HelloApplication.java
+++ src/main/java/com/example/lab2javafx/HelloApplication.java
@@ -4,17 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
+import javafx.scene.control.Spinner;
+
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+
+        IntegerSpinnerValueFactory valueFactory = new IntegerSpinnerValueFactory(1, 1000000);
+        Spinner sp1 = (Spinner) scene.lookup("#cylinderRadius");
+        sp1.setValueFactory(valueFactory);
+        IntegerSpinnerValueFactory valueFactory2 = new IntegerSpinnerValueFactory(1, 1000000);
+        Spinner sp2= (Spinner) scene.lookup("#cylinderHeight");
+        sp2.setValueFactory(valueFactory2);
+        IntegerSpinnerValueFactory valueFactory3 = new IntegerSpinnerValueFactory(1, 1000000);
+        Spinner sp3 = (Spinner) scene.lookup("#cylinderGauge");
+        sp3.setValueFactory(valueFactory3);
+
+        stage.setTitle("Tin cylinder cost");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/lab2javafx/HelloController.java
+++ src/main/java/com/example/lab2javafx/HelloController.java
@@ -4,32 +4,55 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Font;
 
+import java.text.BreakIterator;
+import java.util.Random;
+import java.util.Scanner;
+
 public class HelloController {
-    public Button btnMain;
-    public TextField txtLicznik;
-    int clicCounter=0;
-    int textClicCounter=0;
+
+
+    public Spinner cylinderRadius;
+    public Spinner cylinderHeight;
+    public Spinner cylinderGauge;
+    public Button calculate;
+    public TextField steelMass;
+    public TextField steelPrice;
+    public TextField copperMass;
+    public TextField copperPrice;
 
     @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void btmCalculate(ActionEvent actionEvent) {
+        Integer radius;
+        Integer height;
+        Integer gauge;
+        radius = (Integer) cylinderRadius.getValue();
+        height = (Integer)cylinderHeight.getValue();
+        gauge = (Integer)cylinderGauge.getValue();
+       /* if (radius == null || height == null || gauge == null) return;
+        if (radius <= 0 || height <= 0 || gauge <= 0) return;*/
+        double massCopper;
+        double massSteel;
+        double priceCopper;
+        double priceSteel;
+        final Integer kgPriceCopper = 61;
+        final Integer kgPriceSteel = 2;
+        double _V = radius * radius * (Math.PI) * height;
+        massSteel = _V * gauge * 0.00785;
+        massCopper = _V * gauge * 0.0089;
+        priceSteel = kgPriceSteel * massSteel;
+        priceCopper = kgPriceCopper * massCopper;
+        steelMass.setText(String.valueOf(massSteel));
+        copperMass.setText(String.valueOf(massCopper));
+        steelPrice.setText(String.valueOf(priceSteel));
+        copperPrice.setText(String.valueOf(priceCopper));
     }
 
-    public void onBtnAction(ActionEvent actionEvent) {
-        System.out.println("Kliknięcie nr "+ ++clicCounter);
-        if (clicCounter==1)
-            btnMain.setText("I jeszcze raz");
-    }
-    public void txtLicznik(ActionEvent actionEvent){
-        System.out.println("Kliknięcie nr "+ ++textClicCounter);
-        if (textClicCounter==1)
-            txtLicznik.setText("");
 
-    }
 }
